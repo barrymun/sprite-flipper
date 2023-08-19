8067b78cbe70d2cebf5f51ffc4b5c32ee46273d3
Neil Murphy <barrymun@tcd.ie>
2023-08-19 12:52:29 +0100

    working image upload

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style.css'
+
+const spriteSheetInput = document.getElementById('sprite-input')! as HTMLInputElement;
+const spriteSheetImage = document.getElementById('sprite-img')! as HTMLImageElement;
+
+spriteSheetInput.addEventListener('change', (event: Event) => {
+  if (!(event.target instanceof HTMLInputElement)) return;
+  
+  if (!event.target.files) return;
+  console.log(event.target.files[0])
+  spriteSheetImage.src = URL.createObjectURL(event.target.files[0]);
+  spriteSheetImage.onload = function() {
+    URL.revokeObjectURL(spriteSheetImage.src); // free memory
+  }
+});
