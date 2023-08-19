9872c5c870dd8d37c8f7042cd9bb5d2f818d3247
Neil Murphy <barrymun@tcd.ie>
2023-08-19 14:04:17 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,15 @@ const spriteSheetFlipped = document.getElementById('sprite-flipped')! as HTMLIma
 
 const widthOfOnePiece: number = 150;
 const heightOfOnePiece: number = 150;
+const numCols: number = 8;
+const numRows: number = 1;
 
 spriteSheetInput.addEventListener('change', (event: Event) => {
   if (!(event.target instanceof HTMLInputElement)) return;
   
   if (!event.target.files) return;
+  
   console.log(event.target.files[0])
-  // spriteSheetImage.src = URL.createObjectURL(event.target.files[0]);
-  // spriteSheetImage.onload = function() {
-  //   URL.revokeObjectURL(spriteSheetImage.src); // free memory
-  // }
 
   const reader = new FileReader();
   reader.onload = () => {
@@ -26,27 +25,27 @@ spriteSheetInput.addEventListener('change', (event: Event) => {
   reader.readAsDataURL(event.target.files[0]);
 });
 
-const sliceImage = () => {
-  const imagePieces = [];
-  for(var x = 0; x < 8; ++x) {
-      for(var y = 0; y < 1; ++y) {
-          var canvas = document.createElement('canvas');
-          canvas.width = widthOfOnePiece;
-          canvas.height = heightOfOnePiece;
-          const context = canvas.getContext('2d')!;
-          context.drawImage(
-            spriteSheetImage, 
-            x * widthOfOnePiece, 
-            y * heightOfOnePiece, 
-            widthOfOnePiece, 
-            heightOfOnePiece, 
-            0, 
-            0, 
-            canvas.width, 
-            canvas.height
-          );
-          imagePieces.push(canvas.toDataURL());
-      }
+const sliceImage = (): void => {
+  const imagePieces: string[] = [];
+  for (let x = 0; x < numCols; ++x) {
+    for (let y = 0; y < numRows; ++y) {
+        var canvas: HTMLCanvasElement = document.createElement('canvas');
+        canvas.width = widthOfOnePiece;
+        canvas.height = heightOfOnePiece;
+        const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
+        context.drawImage(
+          spriteSheetImage, 
+          x * widthOfOnePiece, 
+          y * heightOfOnePiece, 
+          widthOfOnePiece, 
+          heightOfOnePiece, 
+          0, 
+          0, 
+          canvas.width, 
+          canvas.height
+        );
+        imagePieces.push(canvas.toDataURL());
+    }
   }
   spriteSheetFlipped.src = imagePieces[0];
 }
